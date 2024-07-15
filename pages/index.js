0da4ae9af0122e75ae5d183176bf09d9f1f95a9b
cyberneel <neelesh.chevuri25@gmail.com>
2024-07-15 10:50:51 +0530

    Added navbar with bootstrap

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,26 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
+    <>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Cyberneel</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     <div className="container">
       <Head>
         <title>Create Next App</title>
@@ -205,5 +225,6 @@ export default function Home() {
         }
       `}</style>
     </div>
+    </>
   )
 }
