3217041fc53ce10e646aa8b7e18ee3fa208b7282
cyberneel <neelesh.chevuri25@gmail.com>
2024-07-15 21:54:07 +0530

    Fixed footer and added spacing (ui tweaks)

--- components/Footer.js
+++ components/Footer.js
@@ -1,35 +1,30 @@
-import Icon from './Icon';
+import * as Icon from 'react-bootstrap-icons';
 
 function Footer() {
   
   
   return(
     <>
-
-  <footer class="bg-dark text-center text-white pt-3">
+    <div class="p-3"></div>
+  <footer class="bg-dark text-center text-white rounded-3">
 
   <div class="container p-4 pb-0">
 
     <section class="mb-4">
 
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><Icon
-      iconName="google"
-      color="royalblue"
-      size={96}
-      className="align-top"
-    />
+        ><Icon.Google />
       </a>
 
 
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fab fa-instagram"></i
-      ></a>
+        ><Icon.Instagram />
+       </a>
 
 
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fab fa-github"></i
-      ></a>
+        ><Icon.Github />
+      </a>
     </section>
 
   </div>
@@ -37,7 +32,7 @@ function Footer() {
 
 
   <div class="text-center p-3 bg-dark" >
-    © 2020 Copyright:&nbsp;
+    © 2024 Copyright:&nbsp;
     <a class="text-white" href="#">Neelesh Chevuri</a>
   </div>
 
--- components/Icon.js
+++
@@ -1,6 +0,0 @@
-import * as icons from "react-bootstrap-icons"
-
-export default Icon = ({ iconName, ...props }) => {
-  const BootstrapIcon = icons[iconName]
-  return (<BootstrapIcon {...props} />)
-}
