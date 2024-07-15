fa3329d4b1b1a7e1213a829f976d61926dd9a641
cyberneel <neelesh.chevuri25@gmail.com>
2024-07-15 22:07:59 +0530

    added background effect

--- pages/_app.js
+++ pages/_app.js
@@ -3,24 +3,47 @@ import '../styles/globals.css'
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import { useRouter } from "next/router"
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
 //import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
     if (typeof document !== undefined) {
       require('bootstrap/dist/js/bootstrap');
     }
+    
+    const handleMouseMove = (event) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
   }, [router.events]);
   
   
   return (
     <>
+    
     <Head>
       <link rel="icon" href="https://github.com/4301e00e-966d-44c8-b913-d06cf7b553b1" type="image/x-icon"/>
     </Head>
+    
+    
+    <div
+        className="background-mask"
+        style={{
+          '--mask-x': `${mousePosition.x}px`,
+          '--mask-y': `${mousePosition.y}px`,
+        }}
+      />
+    
       <Layout>
         <Component {...pageProps} />
       </Layout>
