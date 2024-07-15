2390ee5b73c8a2a418fb00a1179db051ae54ea3a
cyberneel <neelesh.chevuri25@gmail.com>
2024-07-15 11:41:09 +0530

    tried adding js and working on bootstrap navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,57 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Cyberneel</a>
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+//import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  const [pages, setPages] = useState([]);
+
+  useEffect(() => {
+    fetch('/pages.json')
+      .then(response => response.json())
+      .then(data => setPages(data));
+  }, []);
+
+
+  const renderMenuItems = (items) => {
+    return items.map((item, index) => {
+      if (item.test) {
+        return null;
+      }
+      if (item.children) {
+        return (
+          <li key={index} className={'nav-item dropdown'}>
+            <a className={'nav-link dropdown-toggle'}>{item.label}</a>
+            <div className={'dropdown-menu'}>
+              {item.children.map((cItem) => {
+                <a className={'dropdown-item'} href={cItem.path}>{cItem.label}</a>
+              })}
+            </div>
+          </li>
+        );
+      }
+      return (
+        <li key={index} className={'nav-item'}>
+          <Link href={item.path}>
+            <a className={'nav-link'}>{item.label}</a>
+          </Link>
+        </li>
+      );
+    });
+  };
+
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
+      {renderMenuItems(pages)}
     </ul>
   </div>
 </nav>
+  );
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,41 @@
 import 'bootstrap/dist/css/bootstrap.css'
 //import '../styles/globals.css'
 import Head from 'next/head'
+import Layout from '../components/Layout'
+import { useRouter } from "next/router"
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   useEffect(() => {
-    //import("bootstrap/dist/js/bootstrap");
-  }, []);
+    
+    // Ensure scripts are only loaded on the client side
+    if (typeof window !== 'undefined') {
+      const loadScript = (src) => {
+        return new Promise((resolve, reject) => {
+          const script = document.createElement('script');
+          script.src = src;
+          script.onload = resolve;
+          script.onerror = reject;
+          document.body.appendChild(script);
+        });
+      };
 
+      // Load scripts in sequence
+      loadScript('../public/bootstrap.min.js');
+    }
+    
+  }, [router.events]);
+  
+  
   return (
     <>
     <Head>
       <link rel="icon" href="https://github.com/4301e00e-966d-44c8-b913-d06cf7b553b1" type="image/x-icon"/>
     </Head>
-
-      <Component {...pageProps} />
-
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
     );
 }
