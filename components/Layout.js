cdc8ffd180280e594fa4b342a62923d1b75f3dbe
cyberneel <neelesh.chevuri25@gmail.com>
2024-07-17 10:01:04 +0530

    Trying transitions

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,35 @@
 import Navbar from './Navbar';
-import Footer from './Footer'
+import Footer from './Footer';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+
+  const variants = {
+    hidden: { opacity: 0, x: -200, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: -100 },
+  };
+
   return (
     <>
-    <div style={{ padding: '5px' }}>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-    </div>
+      <div style={{ padding: '5px' }}>
+        <Navbar />
+        <AnimatePresence exitBeforeEnter>
+          <motion.main
+            key={router.route}
+            initial="hidden"
+            animate="enter"
+            exit="exit"
+            variants={variants}
+            transition={{ type: 'linear' }}
+          >
+            {children}
+          </motion.main>
+        </AnimatePresence>
+        <Footer />
+      </div>
     </>
   );
 };
